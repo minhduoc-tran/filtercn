c6fd7544aa1630ba767ebd3fdc73167fb9a6315a
minhduoc-tran <tmduoc.169@gmail.com>
2026-02-28 21:54:35 +0700

    chore: fix example e2e tests

--- __tests__/e2e/example.spec.ts
+++ __tests__/e2e/example.spec.ts
@@ -7,9 +7,10 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/Create Next App/);
 });
 
-test("get started link", async ({ page }) => {
+test("hero heading", async ({ page }) => {
   await page.goto("/");
 
-  // Click the get started link.
-  await page.getByRole("heading", { name: "To get started, edit the page.tsx file." }).isVisible();
+  // Check the hero heading
+  const heading = page.getByRole("heading", { name: /The Ultimate\s*Conditional Filter\s*for React/i });
+  await expect(heading).toBeVisible();
 });
